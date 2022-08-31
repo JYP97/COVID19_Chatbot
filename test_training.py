9a4021999bb9123a80a2ef9ba3f7df7658a58241
J.Pan <pjy123@live.com>
2022-08-31 22:43:32 +0200

    pjy

--- test_training.py
+++ test_training.py
@@ -12,55 +12,56 @@ import json
 
 with open("dataset/QA_en.json", encoding='utf-8') as file:
     dialogs = json.load(file)
-print(dialogs)
-# try:
-#     with open("data.pickle", "rb") as f:
-#         words, labels, training, output = pickle.load(f)
-# except:
-words = []
-labels = []
-docs_x = []
-docs_y = []
+# print(dialogs)
 
-for intent in dialogs["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(wrds)
-        docs_y.append(intent["tag"])
+try:
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f)
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-    if intent["tag"] not in labels:
-        labels.append(intent["tag"])
+    for intent in dialogs["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)
+            words.extend(wrds)
+            docs_x.append(wrds)
+            docs_y.append(intent["tag"])
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-words = sorted(list(set(words)))
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
-labels = sorted(labels)
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+    words = sorted(list(set(words)))
 
-training = []
-output = []
+    labels = sorted(labels)
 
-out_empty = [0 for _ in range(len(labels))]
+    training = []
+    output = []
 
-for x, doc in enumerate(docs_x):
-    bag = []
-    wrds = [stemmer.stem(w) for w in doc]
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+    out_empty = [0 for _ in range(len(labels))]
 
-    training.append(bag)
-    output.append(output_row)
+    for x, doc in enumerate(docs_x):
+        bag = []
+        wrds = [stemmer.stem(w) for w in doc]
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-training = np.array(training)
-output = np.array(output)
+        training.append(bag)
+        output.append(output_row)
 
-# with open("data.pickle", "wb") as f:
-#     pickle.dump((words, labels, training, output), f)
+    training = np.array(training)
+    output = np.array(output)
+
+    with open("data.pickle", "wb") as f:
+        pickle.dump((words, labels, training, output), f)
 
 tf.compat.v1.reset_default_graph()
 
@@ -72,11 +73,11 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-# try:
-#     model.load("model.tflearn")
-# except:
-model.fit(training, output, n_epoch=2000, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=2000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
 
 
 def bag_of_words(s, words):
